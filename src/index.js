10855d4d755a341d178fc36e30f63cbb0de28c4c
simtor <simone.ram@ada.ac.uk>
2020-07-07 14:43:34 +0100

    Added validation

--- src/index.js
+++ src/index.js
@@ -12,8 +12,18 @@ rl.question("Please Input the product that your looking for: ", async (answer) =
     var result  = correctAswers.includes(answer);
     if (result){
         console.log("You Product has  been found", answer)
+        rl.close();
     }else{
       console.log("You Product has not  been found", answer)
-
+      rl.setPrompt("Please input product number again: ");
+      console.log(correctAswers);
+      rl.prompt();
+      rl.on('line',(answer)=>{
+        var result  = correctAswers.includes(answer);
+        if(result){
+          console.log("You Product has  been found", answer)
+          rl.close();
+        }
+      })
     }
   });
