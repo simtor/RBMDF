01a747803f10c55852c04d36c8d97c34f456a0db
simtor <simone.ram@ada.ac.uk>
2020-07-07 13:14:45 +0100

    Changed varible

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ const rl = readline.createInterface({
 
 rl.question("Please Input the product that your looking for: ", async (answer) => {
     var correctAswers = ["PL123456", "PL513872", "PL999999", "PL654321", "PL818181"];
-    var n = correctAswers.includes(answer);
-    if (n){
+    var result  = correctAswers.includes(answer);
+    if (result){
         console.log("You Product has  been found", answer)
     }else{
       console.log("You Product has not  been found", answer)
