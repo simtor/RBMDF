d90fd49575b97b927d1ccb89497120efe7be05d3
Simone Ram <simoneram@macbook-pro.home>
2020-07-13 09:12:39 +0100

    validating input

--- src/index.js
+++ src/index.js
@@ -1,40 +1,40 @@
-const readline = require("readline");
+// const readline = require("readline");
  
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
 
 
 
-  //https://www.youtube.com/watch?v=vU6OTnhj3wM
+//   //https://www.youtube.com/watch?v=vU6OTnhj3wM
 
 
-let correctAswers = ["PL123456", "PL513872", "PL999999", "PL654321", "PL818181"]
+// let correctAswers = ["PL123456", "PL513872", "PL999999", "PL654321", "PL818181"]
 
-// TODO: add validation
+// // TODO: add validation
 
-function inputHandler(answer) {
-  let result = correctAswers.includes(answer)
-  if (result){
-        console.log("You Product has  been found", answer)
-        rl.close();
-    }else{
-      console.log("You Product has not  been found", answer)
-      rl.setPrompt("Please input product number again: ");
-      console.log(correctAswers);
-      rl.prompt();
-      rl.on('line',(answer)=>{
-        var result  = correctAswers.includes(answer);
-        if(result){
-          console.log("You Product has  been found", answer)
-          rl.close();
-        }
-      })
-    }
-}
+// function inputHandler(answer) {
+//   let result = correctAswers.includes(answer)
+//   if (result){
+//         console.log("You Product has  been found", answer)
+//         rl.close();
+//     }else{
+//       console.log("You Product has not  been found", answer)
+//       rl.setPrompt("Please input product number again: ");
+//       console.log(correctAswers);
+//       rl.prompt();
+//       rl.on('line',(answer)=>{
+//         var result  = correctAswers.includes(answer);
+//         if(result){
+//           console.log("You Product has  been found", answer)
+//           rl.close();
+//         }
+//       })
+//     }
+// }
 
-rl.question("Input your medical licence number: ", inputHandler)
+// rl.question("Input your medical licence number: ", inputHandler)
 
 
 
@@ -59,3 +59,31 @@ rl.question("Input your medical licence number: ", inputHandler)
 //       })
 //     }
 //   });
+
+
+
+
+
+
+const readline = require('readline');
+const rl = readline.createInterface({
+  input : process.stdin, 
+  output : process.stdout
+});
+
+
+let num1 = Math.floor((Math.random() *10) + 1);
+let num2 = Math.floor((Math.random() *10) + 1);
+let answer = num1 + num2;
+
+rl.question(`What is ${num1} + ${num2}?\n`,
+(userInput)=>{
+  if(userInput.trim() == answer){
+    rl.close()
+  }
+  console.log(userInput);
+
+})
+
+
+
