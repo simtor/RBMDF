bea01b90f055c21479828f5072b496923bd8b7f7
simtor <simone.ram@ada.ac.uk>
2020-07-14 16:05:27 +0100

    Improved validation and input

--- src/asd.js
+++ src/asd.js
@@ -34,6 +34,97 @@ rl.on("close",()=>{
   console.log("correct answer")
 });
 
+
+// function inputValiidation(result){
+//   if (result){
+//     console.log("You Product has  been found", answer)
+//     rl.close();
+//   }
+//   else{
+//     rl.setPrompt("The Medical licence number was not found :(\n");
+//     console.log(correctAswers);
+//     // console.log("Please input The Medical licence number: ")
+//     rl.prompt();
+//     rl.on("line",(answer)=>{
+//       let result = correctAswers.includes(answer)
+//       if(result){
+//         console.log("You Product has  been found", answer)
+//       rl.close();
+//       }   
+//       else{
+//         rl.setPrompt("The Medical licence number was not found :(\n");
+//         console.log(correctAswers);
+//         rl.prompt();
+//       }
+//     })
+//   }
+// }
+
+// //Ah - sort of! Try just adding the bit you’re repeating into the function, i.e. - the part about checking the result and returning true or false. 
+// //You could then wrap that part in a while (resultNotFound) loop so that you don’t need to keep repeating the same code
+// function inputHandler(answer) {
+//   let result = correctAswers.includes(answer)
+//   if(result){
+//     console.log("You Product has been found", answer)
+//   }
+//   else{
+//     inputValiidation();
+//   }
+//   // return result;
+// }
+
+
+// rl.question("Input your medical licence number: ", inputHandler)
+// rl.on("close",()=>{
+//   console.log("Your search as been successfully found the Medical licence number")
+// });
+
+// exports.inputHandler = inputHandler
+
+
+
+
+// rl.question("Please Input the product that your looking for: ", async (answer) => {
+//     var correctAswers = ["PL123456", "PL513872", "PL999999", "PL654321", "PL818181"];
+//     var result  = correctAswers.includes(answer);
+//     if (result){
+//         console.log("You Product has  been found", answer)
+//         rl.close();
+//     }else{
+//       console.log("You Product has not  been found", answer)
+//       rl.setPrompt("Please input product number again: ");
+//       console.log(correctAswers);
+//       rl.prompt();
+//       rl.on('line',(answer)=>{
+//         var result  = correctAswers.includes(answer);
+//         if(result){
+//           console.log("You Product has  been found", answer)
+//           rl.close();
+//         }
+//       })
+//     }
+//   });
+
+
+
+
+// else{
+//     console.log("You Product has not  been found", answer)
+//     rl.setPrompt("Please input product number again: ");
+//     console.log(correctAswers);
+//     rl.prompt();
+//     rl.on('line',(answer)=>{
+//       var result  = correctAswers.includes(answer);
+//       if(result){
+//         console.log("You Product has  been found", answer)
+//         rl.close();
+//       }else{
+//         rl.setPrompt(`Your answer of ${answer} is incorrect\n`)
+//         rl.prompt();
+//       }
+//     })
+//   }
+
 // const readline = require("readline");
  
 // const rl = readline.createInterface({
--- src/index.js
+++ src/index.js
@@ -5,98 +5,25 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-let correctAswers = [name="PL123456", "PL513872", "PL999999", "PL654321", "PL818181"]
+let correctAswers = ["PL123456", "PL513872", "PL999999", "PL654321", "PL818181"]
 
 
-function inputValiidation(result){
-  if (result){
-    console.log("You Product has  been found", answer)
-    rl.close();
-  }
-  else{
-    rl.setPrompt("The Medical licence number was not found :(\n");
-    console.log(correctAswers);
-    // console.log("Please input The Medical licence number: ")
-    rl.prompt();
-    rl.on("line",(answer)=>{
-      let result = correctAswers.includes(answer)
-      if(result){
-        console.log("You Product has  been found", answer)
-      rl.close();
-      }   
-      else{
-        rl.setPrompt("The Medical licence number was not found :(\n");
-        console.log(correctAswers);
-        rl.prompt();
-      }
-    })
-  }
-}
-
-//Ah - sort of! Try just adding the bit you’re repeating into the function, i.e. - the part about checking the result and returning true or false. 
-//You could then wrap that part in a while (resultNotFound) loop so that you don’t need to keep repeating the same code
 function inputHandler(answer) {
   let result = correctAswers.includes(answer)
-  if(result){
+  if (result) {
     console.log("You Product has been found", answer)
+    return true
+  } else {
+    rl.write("The Medical licence number was not found :(\n")
+    rl.question("Input your medical licence number: ", inputHandler)
   }
-  else{
-    inputValiidation();
-  }
-  // return result;
 }
 
-
 rl.question("Input your medical licence number: ", inputHandler)
-rl.on("close",()=>{
-  console.log("Your search as been successfully found the Medical licence number")
-});
 
 exports.inputHandler = inputHandler
 
 
-
-
-// rl.question("Please Input the product that your looking for: ", async (answer) => {
-//     var correctAswers = ["PL123456", "PL513872", "PL999999", "PL654321", "PL818181"];
-//     var result  = correctAswers.includes(answer);
-//     if (result){
-//         console.log("You Product has  been found", answer)
-//         rl.close();
-//     }else{
-//       console.log("You Product has not  been found", answer)
-//       rl.setPrompt("Please input product number again: ");
-//       console.log(correctAswers);
-//       rl.prompt();
-//       rl.on('line',(answer)=>{
-//         var result  = correctAswers.includes(answer);
-//         if(result){
-//           console.log("You Product has  been found", answer)
-//           rl.close();
-//         }
-//       })
-//     }
-//   });
-
-
-
-
-// else{
-//     console.log("You Product has not  been found", answer)
-//     rl.setPrompt("Please input product number again: ");
-//     console.log(correctAswers);
-//     rl.prompt();
-//     rl.on('line',(answer)=>{
-//       var result  = correctAswers.includes(answer);
-//       if(result){
-//         console.log("You Product has  been found", answer)
-//         rl.close();
-//       }else{
-//         rl.setPrompt(`Your answer of ${answer} is incorrect\n`)
-//         rl.prompt();
-//       }
-//     })
-//   }
 
 
 
