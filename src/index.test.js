3eafb0beea250613cecc7d748675098bdfa9a01f
simtor <simone.ram@ada.ac.uk>
2020-07-20 11:18:18 +0100

    testing

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,12 @@
 const inputHandler = require("./index.js")
 
 test("Test function works", ()=>{
-    expect(inputHandler("PL001730050").toBe(3
+    expect(inputHandler("PL001730050").toBe(
+        'name: Ibugel gel',
+        'dose: 10mg',
+        'Substance: Ibuprofen',
+        'url: https://mhraproductsproduction.blob.core.windows.net/docs/54454db7a69e9153ab7302232cdb8ee9230579ca',
+        'You Product has been found PL001730050',
     ))
 })
 
