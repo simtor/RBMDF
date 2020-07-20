9a378bc9355e325531e7a42ea2284b6cecf37cbd
simtor <simone.ram@ada.ac.uk>
2020-07-20 15:51:58 +0100

    Added coverage to  test

--- src/index.test.js
+++ src/index.test.js
@@ -4,15 +4,15 @@ test("Test function works", ()=>{
     expect(inputHandler("PL001730050")).toBe(true)
 })
 
-// test("Test Output", ()=>{
-//     expect(inputHandler("PL001730050")).toBe(
-//         'name: Ibugel gel',
-//         'dose: 10mg',
-//         'Substance: Ibuprofen',
-//         'url: https://mhraproductsproduction.blob.core.windows.net/docs/54454db7a69e9153ab7302232cdb8ee9230579ca',
-//         'You Product has been found PL001730050'
-//     )
-// })
+test("Test Output", ()=>{
+    expect(inputHandler("PL001730050")).toEqual(
+        'name: Ibugel gel',
+        'dose: 10mg',
+        'Substance: Ibuprofen',
+        'url: https://mhraproductsproduction.blob.core.windows.net/docs/54454db7a69e9153ab7302232cdb8ee9230579ca',
+        'You Product has been found PL001730050'
+    )
+})
 
 // describe("Input handler", () => {
 //     test("returns true when product exists", () => {
