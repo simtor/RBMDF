d7eb97a17c981dde1f9cc1a56b3134a8edf932c5
simtor <simone.ram@ada.ac.uk>
2020-07-22 15:32:36 +0100

    Fixed error

--- src/index.test.js
+++ src/index.test.js
@@ -1,34 +1,40 @@
-const inputHandler = require("./index.js")
+// const inputHandler = require("./index.js")
 const productInfo = require("./productInfo.js")
-// test("Test function works", ()=>{
-    
-// })
+
+// describe("Input handler", () => {
+//     test("returns true when product exists", () => {
+//       expect(inputHandler("PL001730050")).toBe(true)
+//     }),
+//       test("returns false when product doesn't exist", () => {
+//         expect(inputHandler("PL001730050")).toBe(false)
+//       })
+//   })
 
 test("PL001730050", ()=>{
     expect(productInfo("PL001730050")).toBe(true)
     // expect(inputHandler("PL001730050")).toMatchSnapshot();
 })
 
-// test("PL001655013", ()=>{
-//     expect(inputHandler("PL001655013")).toBe(true);
-//     // expect(inputHandler("PL001655013")).toMatchSnapshot();
-// })
+test("PL001655013", ()=>{
+    expect(productInfo("PL001655013")).toBe(true);
+    // expect(inputHandler("PL001655013")).toMatchSnapshot();
+})
 
-// test("PL136060206", ()=>{
-//     expect(inputHandler("PL136060206")).toBe(true);
-//     // expect(inputHandler("PL136060206")).toMatchSnapshot();
-// })
+test("PL136060206", ()=>{
+    expect(productInfo("PL136060206")).toBe(true);
+    // expect(inputHandler("PL136060206")).toMatchSnapshot();
+})
 
 
-// test("PL252980158", ()=>{
-//     expect(inputHandler("PL252980158")).toBe(true);
-//     // expect(inputHandler("PL252980158")).toMatchSnapshot();
-// })
+test("PL252980158", ()=>{
+    expect(productInfo("PL252980158")).toBe(true);
+    // expect(inputHandler("PL252980158")).toMatchSnapshot();
+})
 
-// test("PL201170079", ()=>{
-//     expect(inputHandler("PL201170079")).toBe(true);
-//     // expect(inputHandler("PL201170079")).toMatchSnapshot();
-// })
+test("PL201170079", ()=>{
+    expect(productInfo("PL201170079")).toBe(true);
+    // expect(inputHandler("PL201170079")).toMatchSnapshot();
+})
 
 // test("Medical data test", ()=>{
 //     expect(inputHandler("PL001730050")).toBe(true)
@@ -60,11 +66,6 @@ test("PL001730050", ()=>{
 //     )
 // })
 
-// describe("Input handler", () => {
-//     test("returns true when product exists", () => {
-//       expect(inputHandler("PL001730050")).toBe(true)
-//     }),
-//       test("returns false when product doesn't exist", () => {
-//         expect(inputHandler("PL001730050")).toBe(false)
-//       })
-//   })
+// test("Test function works", ()=>{
+    
+// })
