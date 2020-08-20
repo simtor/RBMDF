9f5a77ed75a51b1a90ffc534875f628dd4b182c1
Simone Ram <simoneram@macbook-pro.home>
2020-08-20 16:10:54 +0100

    testing typescript

---
+++ out/test.js
@@ -0,0 +1,5 @@
+var mirror = function (input) {
+    console.log(input);
+};
+mirror("Print this");
+//# sourceMappingURL=test.js.map
---
+++ src_ts/test.ts
@@ -0,0 +1,6 @@
+let mirror = (input:string)=>{
+    console.log(input)
+}
+
+let num = 123
+mirror(num.toString())
