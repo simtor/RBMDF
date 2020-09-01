5996b69a7074db3ebbed121341e54d97f076065a
simtor <simone.ram@ada.ac.uk>
2020-09-01 16:26:46 +0100

    solved module error

---
+++ out/models/index.test.js
@@ -0,0 +1,86 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var index_js_1 = require("./index.js");
+// const inputHandler = require("./index.js")
+// const productInfo = require("./productInfo.js")
+test("PL001730050", function () {
+    expect(index_js_1.default("PL001730050")).toEqual({
+        "Substance": "Ibuprofen",
+        "dose": "10mg",
+        "name": "Ibugel gel",
+        "url": "https://mhraproductsproduction.blob.core.windows.net/docs/54454db7a69e9153ab7302232cdb8ee9230579ca",
+    });
+});
+// test("Snapshot input handler", ()=>{
+//     expect(inputHandler("PL001730050")).toMatchSnapshot();
+// })
+test("PL001655013", function () {
+    expect(index_js_1.default("PL001655013")).toEqual({
+        "Substance": "Paracetamol",
+        "dose": "5mg",
+        "name": "Anadin Extra",
+        "url": "https://mhraproductsproduction.blob.core.windows.net/docs/10c02830183326751553be62be5e2564fb47e970",
+    });
+});
+// test("Snapshot input handler", ()=>{
+//     expect(inputHandler("PL001655013")).toMatchSnapshot();
+// })
+test("PL136060206", function () {
+    expect(index_js_1.default("PL136060206")).toEqual({
+        "Substance": "Tadafil",
+        "dose": "10mg",
+        "name": "Tadalafil 10mg film-coated tablets",
+        "url": "https://mhraproductsproduction.blob.core.windows.net/docs/a1744b861fbda647f286cc70865d30cf2673c077",
+    });
+});
+// test("Snapshot input handler", ()=>{
+//     expect(inputHandler("PL136060206")).toMatchSnapshot();
+// })
+// test("PL136060206", ()=>{
+//     expect(productInfo("PL136060206")).toBe(true);
+//     expect(productInfo("PL136060206")).toMatchSnapshot();
+// })
+// test("PL252980158", ()=>{
+//     expect(productInfo("PL252980158")).toBe(true);
+//     expect(productInfo("PL252980158")).toMatchSnapshot();
+// })
+// test("PL201170079", ()=>{
+//     expect(productInfo("PL201170079")).toBe(true);
+//     expect(productInfo("PL201170079")).toMatchSnapshot();
+// })
+// test("Medical data test", ()=>{
+//     expect(inputHandler("PL001730050")).toBe(true)
+//     except(inputHandler(property)).toEqual([
+//         'Ibugel gel'
+//     ]);
+// });
+//   "PL201170079": PL201170079
+// test("Test Output 3", ()=>{
+//     expect(inputHandler(result)).toBe(
+//         'name: Ibugel gel',
+//         'dose: 10mg',
+//         'Substance: Ibuprofen',
+//         'url: https://mhraproductsproduction.blob.core.windows.net/docs/54454db7a69e9153ab7302232cdb8ee9230579ca',
+//         'You Product has been found PL001730050'
+//     );
+// })
+// test("Test Output", ()=>{
+//     expect(inputHandler("PL001730050")).toEqual(
+//         'name: Ibugel gel',
+//         'dose: 10mg',
+//         'Substance: Ibuprofen',
+//         'url: https://mhraproductsproduction.blob.core.windows.net/docs/54454db7a69e9153ab7302232cdb8ee9230579ca',
+//         'You Product has been found PL001730050'
+//     )
+// })
+// test("Test function works", ()=>{
+// })
+// describe("Input handler", () => {
+//     test("returns true when product exists", () => {
+//       expect(inputHandler("PL001730050")).toBe(true)
+//     }),
+//       test("returns false when product doesn't exist", () => {
+//         expect(inputHandler("PL001730050")).toBe(false)
+//       })
+//   })
+//# sourceMappingURL=index.test.js.map
---
+++ out/repositories/medicineInfo.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var PL001730050 = { name: 'Ibugel gel', dose: '10mg', Substance: 'Ibuprofen', url: 'https://mhraproductsproduction.blob.core.windows.net/docs/54454db7a69e9153ab7302232cdb8ee9230579ca' };
+var PL001655013 = { name: 'Anadin Extra', dose: '5mg', Substance: 'Paracetamol', url: 'https://mhraproductsproduction.blob.core.windows.net/docs/10c02830183326751553be62be5e2564fb47e970' };
+var PL136060206 = { name: 'Tadalafil 10mg film-coated tablets', dose: '10mg', Substance: 'Tadafil', url: 'https://mhraproductsproduction.blob.core.windows.net/docs/a1744b861fbda647f286cc70865d30cf2673c077' };
+var PL252980158 = { name: 'Nabilone 1mg capsules', dose: '1mg', Substance: 'Nabilone', url: 'https://mhraproductsproduction.blob.core.windows.net/docs/b68e1e028e44806a00a65d016aec4f1c90436ac4' };
+var PL201170079 = { name: 'Ramipril 2.5mg tablets', dose: '2.5mg', Substance: 'Ramipril', url: 'https://mhraproductsproduction.blob.core.windows.net/docs/bdc8d29cd2957f0ffdd230f6f47a5d9cf5b3f039' };
+var products = {
+    "PL001730050": PL001730050,
+    "PL001655013": PL001655013,
+    "PL136060206": PL136060206,
+    "PL252980158": PL252980158,
+    "PL201170079": PL201170079
+};
+exports.default = products;
+//# sourceMappingURL=medicineInfo.js.map
