eefa006b933c2b9b7e4f21a7833e27c675068ed9
Simone Ram <simoneram@macbook-pro.home>
2020-09-03 15:08:34 +0100

    package.json changes

--- out/app-server.js
+++ out/app-server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var port = 2480;
 app.get('/', function (req, res) {
-    res.send('Welcome to the Drug finder!');
+    res.send('Welcome to the Drug finders!');
 });
 app.listen(port, function () {
     console.log("Example app listening at http://localhost:" + port);
