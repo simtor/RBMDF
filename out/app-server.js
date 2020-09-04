2b313a86cfc17db7033d51928d304441e61f6f21
Simone Ram <simoneram@macbook-pro.home>
2020-09-04 10:48:00 +0100

    fixing error

--- out/app-server.js
+++ out/app-server.js
@@ -4,6 +4,9 @@ var port = 2480;
 app.get('/', function (req, res) {
     res.send('Welcome to the Drug finder!');
 });
+app.get('/', function (req, res) {
+    res.render("index", { title: "I'm using pug", message: "Wow lovely" });
+});
 app.set("title", "Food finder");
 app.set("view engine", "pug");
 app.set("view", "./out/view");
--- src/app-server.ts
+++ src/app-server.ts
@@ -6,11 +6,18 @@ app.get('/', (req, res) => {
   res.send('Welcome to the Drug finder!')
 })
 
+app.get('/', (req, res) => {
+  res.render("index",{title: "I'm using pug", message:"Wow lovely" })
+})
+
+
 
 app.set("title","Food finder")
 
 app.set("view engine", "pug")
 app.set("view", "./out/view")
+
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
