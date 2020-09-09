182797c6c7d78467525efa035e284aaf44057327
simtor <simone.ram@ada.ac.uk>
2020-09-09 15:43:59 +0100

    build as simple component

--- frontend-ts/src/App.tsx
+++ frontend-ts/src/App.tsx
@@ -2,10 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+interface HeaderProps{
+  name: string
+  age: number  
+}
+
+function Header(props: HeaderProps){
+  return(
+    <h1>Hello{props.name}. your{props.age}</h1>
+  )
+}
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Header name="Simone" age={19}/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
