2fbc791ad0ef0165a3f0ac0d8ae129d593654e60
simtor <simone.ram@ada.ac.uk>
2020-09-11 09:55:06 +0100

    made a hook

--- frontend-ts/src/components/Foodsearch/index.tsx
+++ frontend-ts/src/components/Foodsearch/index.tsx
@@ -11,12 +11,24 @@ color: blue;
 
 
 export default function DrugSearch(){
+    // let searchDrug = useState("Beta")
+    let [searchDrug, setsearchDrug] = useState("Beta")
+    let [searchDrugs, setsearchDrugs] = useState([])
+
+    const updateDrug = ()=>{
+        console.log("Updatedx")
+        setsearchDrug("Sorbitan")
+    }
     return(
+        <div>
         <form> <label>Search for Drugs:
         <input type="text" name="drug" id="drug"></input>
         </label>
         <input type="submit" name="submit"></input>
         </form>
+        <button onClick={updateDrug}>Click me</button>
+    <p>{searchDrug}</p>
+    </div>
     //   <StyledH1>Hello{props.name}. your{props.age}</StyledH1>
     )
   }
