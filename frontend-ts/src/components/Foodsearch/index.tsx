ebafcc27c2ed99ac3ed65f2a2f970cdd071b613c
simtor <simone.ram@ada.ac.uk>
2020-09-11 11:35:21 +0100

    Added list

--- frontend-ts/src/components/Foodsearch/index.tsx
+++ frontend-ts/src/components/Foodsearch/index.tsx
@@ -12,8 +12,8 @@ color: blue;
 
 export default function DrugSearch(){
     // let searchDrug = useState("Beta")
-    let [searchDrug, setsearchDrug] = useState("Beta")
-    let [searchDrugs, setsearchDrugs] = useState([])
+    let [searchDrug, setsearchDrug] = useState("")
+    let [searchDrugs, setsearchDrugs] = useState<string[]>([])
 
     // const updateDrug = ()=>{
     //     console.log("Input have been chnaged")
@@ -22,20 +22,32 @@ export default function DrugSearch(){
     // }
 
     const updateDrug = (e: FormEvent<HTMLInputElement>)=>{
-        console.log(e)
+        console.log(e.currentTarget.value)
+        setsearchDrug(e.currentTarget.value)
         // console.log("Input have been chnaged")
         // console.log("Updatedx")
         // setsearchDrug("Sorbitan")
     }
+    const handlesubmit =(e: FormEvent<HTMLFormElement>) =>{
+        e.preventDefault()
+        searchDrugs.push(searchDrug)
+        setsearchDrugs(searchDrugs)
+        setsearchDrug("")
+
+    }
     return(
         <div>
-        <form> <label>Search for Drugs:
+        <form onSubmit={handlesubmit}><label>Search for Drugs:
         <input type="text" name="drug" id="drug" value={searchDrug} onChange={updateDrug}/>
         </label>
         <input type="submit" name="submit"></input>
         </form>
         {/* <button onClick={updateDrug}>Click me</button> */}
-    <p>{searchDrug}</p>
+        <div>
+            <h2>Search drugs</h2>
+        {searchDrugs.map(drugs => <p>{drugs}</p>)}
+        </div>
+
     </div>
     //   <StyledH1>Hello{props.name}. your{props.age}</StyledH1>
     )
